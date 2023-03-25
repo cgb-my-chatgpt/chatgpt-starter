61485a4cefd2057bbfc13476f8097874240694b9
akingde <akingde@sina.com>
2023-03-25 10:12:28 +0800

    调整规范

--- chatgpt-spring-boot-autoconfigure/src/main/java/com/cgb/chatgpt/config/auto/ChatGptAutoImportSelector.java
+++ chatgpt-spring-boot-autoconfigure/src/main/java/com/cgb/chatgpt/config/auto/ChatGptAutoImportSelector.java
@@ -6,6 +6,6 @@ import org.springframework.core.type.AnnotationMetadata;
 public class ChatGptAutoImportSelector implements ImportSelector {
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{"ChatGptAutoConfiguration"};
+        return new String[]{"com.cgb.chatgpt.config.auto.ChatGptAutoConfiguration"};
     }
 }
